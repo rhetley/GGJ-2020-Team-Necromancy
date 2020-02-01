20758d05176a0d073d81a601883bd0c110389009
Richard Hetley (RIT Student) <rsh5353@ad.rit.edu>
2020-02-01 16:31:01 -0500

    No camera zoom, but first tiles

---
+++ Nekomancy/Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    public GameObject defaultCamera;
+    public GameObject runCamera;
+    public int activeCamera;
+    public enum CameraState { Default, TransitionToRun, Run, TransitionToDefault };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultCamera.SetActive(true);
+        runCamera.SetActive(false);
+        activeCamera = (int)CameraState.Default;
+    }
+
+    public void SwitchToDefaultCamera()
+    {
+        defaultCamera.SetActive(true);
+        runCamera.SetActive(false);
+        activeCamera = (int)CameraState.Default;
+    }
+
+    public void SwitchToRunCamera()
+    {
+        defaultCamera.SetActive(false);
+        runCamera.SetActive(true);
+        activeCamera = (int)CameraState.Run;
+    }
+}
