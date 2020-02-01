dcd78443e30f7bbbb6d47e1667aac8dd733ff0b7
Richard Hetley (RIT Student) <rsh5353@ad.rit.edu>
2020-02-01 13:34:51 -0500

    Basic movement and ground checking

---
+++ Nekomancy/Assets/Scripts/PlayerWalkJump.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalkJump : MonoBehaviour
+{
+    public Vector2 velocity;
+    float moveMultiplier = 10.0f;
+    float jumpHeight = 5.0f;
+    Rigidbody2D playerRigidbody;
+    private GroundCheck groundChecker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        groundChecker = GetComponent<GroundCheck>();
+
+        Physics2D.gravity = new Vector2(0.0f, -98.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && groundChecker.CheckIfGrounded())
+        {
+            Jump();
+        }
+        else
+        {
+            velocity.x = Input.GetAxis("Horizontal");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        playerRigidbody.velocity = new Vector2(velocity.x * moveMultiplier, GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump()
+    {
+        velocity.y = Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y));
+        playerRigidbody.velocity = new Vector2(velocity.x, velocity.y);
+        velocity.y += Physics2D.gravity.y * Time.deltaTime;
+        playerRigidbody.velocity = new Vector2(velocity.x, velocity.y);
+    }
+}
+
