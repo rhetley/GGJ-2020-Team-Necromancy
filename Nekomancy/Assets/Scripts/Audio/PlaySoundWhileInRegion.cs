8a79ef5b0e2459f546eb47e8c6ef9e990d4c1776
unknown <IGMAdmin@IGM2025-32.main.ad.rit.edu>
2020-02-01 19:34:08 -0500

    Sound Controller

---
+++ Nekomancy/Assets/Scripts/Audio/PlaySoundWhileInRegion.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundWhileInRegion : MonoBehaviour
+{
+    public SoundId SoundToPlay;
+    public string SoundManagerName = "SoundManager";
+    public GameObject GameObject;
+
+    private SoundController soundController;
+
+
+    private void Awake()
+    {
+        GameObject SoundManager = GameObject.Find(SoundManagerName);
+        soundController = SoundManager.GetComponent<SoundController>();
+
+        GameObject = GetComponent<GameObject>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject  == GameObject)
+        {
+            soundController.Play(SoundToPlay);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == GameObject)
+        {
+            soundController.Stop(SoundToPlay);
+        }
+    }
+}
