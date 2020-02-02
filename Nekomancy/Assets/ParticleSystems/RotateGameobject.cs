f85f7ccfa65e692a2e2b716009ac2bdf2258145f
Jack Levitt (RIT Student) <jel6623@ad.rit.edu>
2020-02-01 19:42:11 -0500

    hey

---
+++ Nekomancy/Assets/ParticleSystems/RotateGameobject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGameobject : MonoBehaviour
+{
+    [SerializeField]
+    float radius;
+    [SerializeField]
+    float speed;
+    void Update()
+    {
+        this.transform.position = new Vector3(radius * Mathf.Cos(speed*Time.time), radius * Mathf.Sin(speed*Time.time),0);
+    }
+}
